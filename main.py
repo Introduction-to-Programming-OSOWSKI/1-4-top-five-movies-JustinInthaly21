110bbff788dddcc4bb11e2e8c36697ba4b6e3579
JustinInthaly21 <825134@mystma.org>
2020-12-17 18:14:41 +0000

    topfivemovies 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+def topFiveMovies(a,  b,  c,  d,  e):
+    return a + " " + b + " " + c + " " + d + " " + e
 
 print (topFiveMovies("star wars empire strikes back",  "the maze runner",  "the purge",  "fast and furious",  "shrek"))
